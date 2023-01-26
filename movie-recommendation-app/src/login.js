2dfa2ea6a02bcd7806c95fa034484c180aca964c
Ermiyoo <Ermiyoo@protonmail.com>
2023-01-26 11:37:30 -0500

    changing the component files

--- movie-recommendation-app/src/login.js
+++ movie-recommendation-app/src/login.js
@@ -1,19 +1,186 @@
-import {TextInputHolder,LoginButton,SignUpLink} from "./login_componenets";
-import "./login.css"
-function Login(){
-    return(
-    <>
-    <center>
-    <div class="container">
-    <img src={require(".//image_asset/logo.png")} width="100" height="100"></img><br/><br/>
-      <TextInputHolder type="email" name="email" hint="User Email"></TextInputHolder><br/>
-      <TextInputHolder type="password" name="password" hint="User Password"></TextInputHolder><br/>
-      <LoginButton ></LoginButton><br/><br/>
-      <SignUpLink></SignUpLink>
-     </div>
-     </center>
-    </>
+import React from "react";
+import { TextInputHolder, LoginButton, SignUpLink } from "./login_componenets";
+import axios from "axios";
+import bcrypt from "bcryptjs";
+import "./login.css";
+
+let salt = bcrypt.genSaltSync(10);
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  login = () => {
+    // const pwd = bcrypt.hashSync(this.state.password, salt);
+
+    axios
+      .post("http://localhost:8000/api/v1/user/login", {
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("user_id", res.data.user._id);
+      })
+      .catch((err) => {
+        if (
+          err.response &&
+          err.response.data &&
+          err.response.data.errorMessage
+        ) {
+          alert("User Not Found");
+        }
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <center>
+          <div class="container">
+            <img
+              src={require(".//image_asset/logo.png")}
+              width="100"
+              height="100"
+              alt="logo"
+            ></img>
+            <br />
+            <br />
+            <TextInputHolder
+              value={this.state.username}
+              onChange={this.onChange}
+              type="text"
+              name="username"
+              hint="Username"
+            ></TextInputHolder>
+            <br />
+            <TextInputHolder
+              value={this.state.password}
+              onChange={this.onChange}
+              type="password"
+              name="password"
+              hint="Password"
+            ></TextInputHolder>
+            <br />
+            <LoginButton></LoginButton>
+            <br />
+            <br />
+            <SignUpLink></SignUpLink>
+          </div>
+        </center>
+      </>
     );
+  }
 }
 
-export default Login
+/*
+
+import "./login.css";
+import React from "react";
+import { Button, TextField, Link, FormHelperText } from "@material-ui/core";
+import axios from "axios";
+import bcrypt from "bcryptjs";
+import "./login.css";
+import Register from "./components/signUp";
+
+let salt = bcrypt.genSaltSync(10);
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  login = () => {
+    // const pwd = bcrypt.hashSync(this.state.password, salt);
+
+    axios
+      .post("http://localhost:8000/api/v1/user/login", {
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("user_id", res.data.user._id);
+      })
+      .catch((err) => {
+        if (
+          err.response &&
+          err.response.data &&
+          err.response.data.errorMessage
+        ) {
+          alert("User Not Found");
+        }
+      });
+  };
+
+  render() {
+    return (
+      <div className="mainContainer">
+        <div>
+          <h2>Login</h2>
+        </div>
+
+        <div>
+          <TextField
+            id="standard-basic"
+            type="text"
+            autoComplete="off"
+            name="username"
+            value={this.state.username}
+            onChange={this.onChange}
+            placeholder="Username"
+            required
+          />
+          <br />
+          <br />
+          <TextField
+            id="standard-basic"
+            type="password"
+            autoComplete="off"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChange}
+            placeholder="Password"
+            required
+          />
+          <br />
+          <br />
+          <Button
+            id="logbtn"
+            className="button_style"
+            variant="contained"
+            color="primary"
+            size="small"
+            disabled={this.state.username === "" && this.state.password === ""}
+            onClick={this.login}
+          >
+            Login
+          </Button>{" "}
+          <Link id="reg" href="/register">
+            Register
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+*/
+
+// export default Login
--- movie-recommendation-app/src/login_componenets.js
+++ movie-recommendation-app/src/login_componenets.js
@@ -1,46 +1,54 @@
 import Sign_up_page from "./sign_up_page";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import User_home_page from "./user_home_page";
-function TextInputHolder(props){
-    return(
+function TextInputHolder(props) {
+  return (
     <>
-    <input type={props.type} class={props.name} placeholder={props.hint}/>
+      <input
+        type={props.type}
+        className={props.name}
+        placeholder={props.hint}
+      />
     </>
-    );
+  );
 }
 //to redirect to user home page you have to make sure password and email before redirecting
-function home_page(){
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-       <User_home_page></User_home_page>
-      </React.StrictMode>
-    );
+function home_page() {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <User_home_page></User_home_page>
+    </React.StrictMode>
+  );
 }
 
-function LoginButton(){
-    return(
-        <>
-        <button  onClick={home_page}class="loginButton" >Login</button>
-        </>
-    );
+function LoginButton() {
+  return (
+    <>
+      <button onClick={home_page} className="loginButton">
+        Login
+      </button>
+    </>
+  );
 }
 // to redirect form login page to sign in page
-function signUp_page(){
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-       <Sign_up_page></Sign_up_page>
-      </React.StrictMode>
-    );
+function signUp_page() {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <Sign_up_page />
+    </React.StrictMode>
+  );
 }
-function SignUpLink(){
-    return(
-        <>
-        <a class="signUpLink"  onClick={signUp_page}>Sign in ?</a>
-        </>
-    );
+function SignUpLink() {
+  return (
+    <>
+      <a className="signUpLink" onClick={signUp_page}>
+        Sign in ?
+      </a>
+    </>
+  );
 }
 
-export {TextInputHolder,LoginButton,SignUpLink}
+export { TextInputHolder, LoginButton, SignUpLink };
