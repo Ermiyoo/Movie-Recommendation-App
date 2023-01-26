ad69c665601dabc943b44eac04be2ce92a68942a
Ermiyoo <Ermiyoo@protonmail.com>
2023-01-26 11:46:00 -0500

    Edited the id

--- without_backend/movie-recommendation-app/src/login.js
+++ without_backend/movie-recommendation-app/src/login.js
@@ -1,19 +1,41 @@
-import {TextInputHolder,LoginButton,SignUpLink} from "./login_componenets";
-import "./login.css"
-function Login(props){
-    return(
+import { TextInputHolder, LoginButton, SignUpLink } from "./login_componenets";
+import "./login.css";
+function Login(props) {
+  return (
     <>
-    <center>
-    <div class="container">
-    <img src={require(".//image_asset/logo.png")} width="100" height="100"></img><br/><br/>
-      <TextInputHolder type="email" name="email" hint="User Email"></TextInputHolder><br/>
-      <TextInputHolder type="password" name="password" hint="User Password"></TextInputHolder><br/>
-      <LoginButton userEmail={props.email} userPassword={props.password}></LoginButton><br/><br/>
-      <SignUpLink></SignUpLink>
-     </div>
-     </center>
+      <center>
+        <div class="container">
+          <img
+            src={require(".//image_asset/logo.png")}
+            width="100"
+            alt="logo"
+            height="100"
+          ></img>
+          <br />
+          <br />
+          <TextInputHolder
+            type="email"
+            name="email"
+            hint="User Email"
+          ></TextInputHolder>
+          <br />
+          <TextInputHolder
+            type="password"
+            name="password"
+            hint="User Password"
+          ></TextInputHolder>
+          <br />
+          <LoginButton
+            userEmail={props.email}
+            userPassword={props.password}
+          ></LoginButton>
+          <br />
+          <br />
+          <SignUpLink></SignUpLink>
+        </div>
+      </center>
     </>
-    );
+  );
 }
 
-export default Login
+export default Login;
